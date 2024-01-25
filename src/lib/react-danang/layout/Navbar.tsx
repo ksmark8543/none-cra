f8938840776f1bc0af0aab3b69c1c61bee9e4ae5
unknown <ksmark8543@naver.com>
2024-01-25 23:57:08 +0900

    .

---
+++ src/lib/react-danang/layout/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { MdOutlineMenu } from "react-icons/md";
+
+interface NavbarProps {
+  title?: React.ReactNode;
+  userName?: React.ReactNode;
+  logout?: React.ReactNode;
+  onMenuClick?: React.MouseEventHandler<SVGElement> | undefined;
+}
+
+const Navbar = ({ title, userName, logout, onMenuClick }: NavbarProps) => {
+  return (
+    <section className={`flex justify-between p-5 transition-all`}>
+      <div className="flex items-center gap-2">
+        <MdOutlineMenu
+          className="cursor-pointer"
+          size={30}
+          onClick={onMenuClick}
+        />
+        <span>{title}</span>
+      </div>
+      <div className="flex items-center gap-5">
+        <div className="flex items-center gap-1 text-sm font-bold text-slate-700">
+          {userName}
+        </div>
+        {logout}
+      </div>
+    </section>
+  );
+};
+export default Navbar;
---
+++ src/lib/react-danang/layout/index.js
@@ -0,0 +1,8 @@
+import MenuGroup from "./MenuGroup";
+import Menu from "./Menu";
+import Navbar from "./Navbar";
+export default {
+  MenuGroup,
+  Menu,
+  Navbar,
+};
