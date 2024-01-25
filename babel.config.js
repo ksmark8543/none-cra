f6a6a1bdef09c51e61e9eadd1c2d11441be2ec2a
unknown <ksmark8543@naver.com>
2024-01-25 22:42:10 +0900

    최초구성

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+};
+// babel/preset-env : ES6 이상의 문법을 ES5 이하로 변환해 준다.
+// babel/preset-react : jsx 문법을 js 코드로 변환해준다.
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+  return <div className="text-lg">Hello world</div>;
+};
+
+export default App;
